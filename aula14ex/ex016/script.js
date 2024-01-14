a701f240dc32ce5ff5719f050b7064ec7d347e82
RenanSaravalli <renansaravalli@gmail.com>
2024-01-14 01:21:30 -0300

    atualizando ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,8 +1,19 @@
-function verificar(){
-    var ini = document.getElementById('txtinicio')
-    var fim = document.getElementById('txtfim')
-    var pas = document.getElementById('txtpas')
-    var res = document.getElementById('res')
-    var res = ini + fim
-    res.innerHTML(`${res}`)
+function contar() {
+    let ini = document.getElementById('txtinicio')
+    let fim = document.getElementById('txtfim')
+    let pas = document.getElementById('txtpas')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} `
+        }
+    }
 }
