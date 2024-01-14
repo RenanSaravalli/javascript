8c02e846d0ab6bed68ec19d053a44d268dfcc347
RenanSaravalli <renansaravalli@gmail.com>
2024-01-14 18:21:02 -0300

    Atualizando ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,15 +5,30 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
+
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pas.value)
-
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c} `
+        if (p <= 0) {
+            window.alert('Passo inválido!')
+            p = 1
         }
+        if (i < f) {
+            //contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        } else {
+            //contagem regressiva
+            for (c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+
     }
 }
